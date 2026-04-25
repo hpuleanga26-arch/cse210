351339982218c0671885afff81f404aa92a78fa2
JP Zavier Siunipa <jpzaviersiunipa@JPs-Mac-mini.local>
2026-04-26 04:50:12 +1300

    Finished prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,90 @@
 using System;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+    {   // Ask the user for percentage
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        // convert answer to intergers by using parse.
+        int percent = int.Parse(answer);
+
+        string score = "";
+        // Using if or else or else if to form a set of conversation.
+        if (percent >= 90)
+        {
+            score = "A";
+            if (percent >= 97)
+            {
+                score = "A";
+            }
+            else
+            {
+                score = "A-";
+            }
+        }
+        else if (percent >= 80)
+        {
+            score = "B";
+
+            if (percent >= 87)
+            {
+                score = "B+";
+            }
+            else
+            {
+                score = "B-";
+            }
+        }
+        else if (percent >= 70)
+        {  
+            score = "C";
+
+            if (percent >= 77)
+            {
+                score = "C+";
+            }
+            else
+            {
+                score = "C-";
+            }  
+        }
+
+        else if (percent >= 60)
+        {
+          score = "D";
+
+          if (percent >= 67)
+            {
+                score = "D+";
+            }
+            else
+            {
+                score = "D-";
+            }
+        }
+        else
+        { 
+            score = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {score
+        }");
+
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulation You Pass");
+        }
+        else
+        {
+            Console.WriteLine("Sorry You Fail, Try again.");
+        }
+
+
+
+
+
+    
     }
 }
